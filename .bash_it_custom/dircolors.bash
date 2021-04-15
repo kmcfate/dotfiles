9ba6973be8812086e8b4840b0ec4f3bbc27b73d7
Kal McFate <kmcfate@amazon.com>
2021-04-15 14:21:15 -0700

    Better dircolors

--- .bash_it_custom/dircolors.bash
+++ .bash_it_custom/dircolors.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [[ -d "$HOME/.solarized/dircolors-solarized" ]]
 then
-  eval `dircolors ~/.solarized/dircolors-solarized/dircolors.256dark`
+  eval `dircolors ~/.solarized/dircolors-solarized/dircolors.ansi-universal`
 fi
 
 alias ls='ls --color=auto'
