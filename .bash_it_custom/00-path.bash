baaa1dd5d4bbe1492523fa2397e7694b2b40f291
Kal McFate <kmcfate@darkink.com>
2023-11-24 11:48:32 -0700

    Update path again to fix idempotency

--- .bash_it_custom/00-path.bash
+++ .bash_it_custom/00-path.bash
@@ -1,3 +1,3 @@
-PATH="/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/sbin:/usr/sbin:/usr/games:/snap/bin:$(gem environment home)/bin"
-echo "$PATH" | grep -q linuxbrew || eval "$(~/.linuxbrew/bin/brew shellenv)"
-PATH="$HOME/bin:$PATH"
+PATH="/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/sbin:/usr/sbin:/usr/games:/snap/bin"
+eval "$(~/.linuxbrew/bin/brew shellenv)"
+PATH="$HOME/bin:$PATH:$(gem environment home)/bin"
