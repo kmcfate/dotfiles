f6a74b0fce20d4163fa23e6e1126a6b0cdb803d4
Kal McFate <kmcfate@darkink.com>
2023-11-28 09:55:53 -0700

    Only add gem to path if gem exists

--- .bash_it_custom/00-path.bash
+++ .bash_it_custom/00-path.bash
@@ -1,3 +1,4 @@
 PATH="/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/sbin:/usr/sbin:/usr/games:/snap/bin"
 eval "$(~/.linuxbrew/bin/brew shellenv)"
-PATH="$HOME/bin:$PATH:$(gem environment home)/bin"
+which gem > /dev/null && PATH="$PATH:$(gem environment home)/bin"
+PATH="$HOME/bin:$PATH"
